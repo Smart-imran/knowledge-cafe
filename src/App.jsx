651a48270de8bcc6cf602bdb358c01d516e4e55c
Smart-imran <rst.imran4@gmail.com>
2024-09-09 23:44:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-red-400 text-center'>Knowladge-cafe</h1>
+     
+    </>
+  )
+}
+
+export default App
